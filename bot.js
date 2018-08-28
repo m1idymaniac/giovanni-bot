281c7c4309adf2543eead88521a2751753494b1d
m1idymaniac <42757006+m1idymaniac@users.noreply.github.com>
2018-08-28 02:41:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
 //Filesystem
-fs.readdir("./commands/", (err, files) => {
+fs.readdir("./Commands/", (err, files) => {
 
   if(err) console.log(err);
 
