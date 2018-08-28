6810dee09574c758d6634cc20c12c19f5035583f
m1idymaniac <42757006+m1idymaniac@users.noreply.github.com>
2018-08-28 10:57:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
-const bot = new Discord.Client();
+const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
 //Filesystem
@@ -9,18 +9,19 @@ fs.readdir("./commands/", (err, files) => {
 
   if(err) console.log(err);
 
-  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
   if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
+    console.log("Couldn't find commands.")
     return;
   }
 
-jsfile.forEach((f, i) =>{
-  let props = require(`./commands/${f}`);
-  console.log(`${f} loaded!`);
-  bot.commands.set(props.help.name, props);
-});
-});
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+
+})
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online`)
@@ -41,13 +42,12 @@ bot.on("message", async message => {
     return message.channel.send("WHAT! This cannot be!");
   }
 
-  let prefix = config.prefix;
-  let content = message.content.split(" ");
-  let command = content[0].toLowerCase();
-  let args = content.slice(1);
-  if(!message.content.startsWith(prefix)) return;
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
 
-  let commandfile = bot.commands.get(command.slice(prefix.length));
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
 
 });
